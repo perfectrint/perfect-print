bee7c446365292485f0483743c5e444ff4412541
perfectrint <kotzrj@chairo.vic.edu.au>
2026-08-06 08:40:36 +1000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
+// Shopping Cart
 
+let cart = [];
+
+// Add product to cart
+function addToCart(productName, price) {
+
+    cart.push({
+        name: productName,
+        price: price
+    });
+
+    updateCart();
+
+}
+
+// Update cart number
+function updateCart() {
+
+    document.getElementById("cartCount").textContent = cart.length;
+
+}
