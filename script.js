f5ef9105f2ff7a7774db8b0a5c8162d5ebbfb42f
perfectrint <kotzrj@chairo.vic.edu.au>
2026-08-06 11:45:04 +1000

    Update script.js

--- script.js
+++ script.js
@@ -17,6 +17,43 @@ function addToCart(productName, price) {
 // Update cart number
 function updateCart() {
 
+    // Update cart number
     document.getElementById("cartCount").textContent = cart.length;
+
+    // Get cart elements
+    const cartItems = document.getElementById("cartItems");
+    const cartTotal = document.getElementById("cartTotal");
+
+    // Clear old items
+    cartItems.innerHTML = "";
+
+    let total = 0;
+
+    // Empty cart
+    if(cart.length === 0){
+
+        cartItems.innerHTML = "<p>Your cart is empty.</p>";
+        cartTotal.textContent = "0";
+        return;
+
+    }
+
+    // Add each product
+    cart.forEach(item =>{
+
+        total += item.price;
+
+        cartItems.innerHTML += `
+            <div style="padding:12px;border-bottom:1px solid #ddd;">
+                <strong>${item.name}</strong><br>
+                $${item.price}
+            </div>
+        `;
+
+    });
+
+    cartTotal.textContent = total;
+
+}
 
 }
